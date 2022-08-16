42ceaa3f3cc750109cbb327f889f47515504f76f
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-08-16 15:56:44 +0900

    DND

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import styled from "styled-components";
 import { toDoState } from "./components/atoms";
 import { useRecoilState } from "recoil";
 import DragabbleCard from "./Components/DragabbleCard";
-
+import Board from "./Components/Board";
 const Wrapper = styled.div`
   display: flex;
   max-width: 480px;
@@ -28,50 +28,33 @@ const Boards = styled.div`
   grid-template-columns: repeat(1, 1fr);
 `;
 
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
-`;
-
 export default function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     // 얘 사용하면 이제 DND해도 원상복구 X
     // source는 방금 건든 애의 index (= 어디서 왔는지)
     // destination은 단어 뜻 그대로 목적지
-    setToDos((oldToDos) => {
-      const toDosCopy = [...oldToDos];
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
-      toDosCopy.splice(source.index, 1);
-      console.log("Deleted item");
-      console.log(toDosCopy);
-      // 2) Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
-      return toDosCopy;
-    });
+    // setToDos((oldToDos) => {
+    //   const toDosCopy = [...oldToDos];
+    //   console.log("Delete item on", source.index);
+    //   console.log(toDosCopy);
+    //   toDosCopy.splice(source.index, 1);
+    //   console.log("Deleted item");
+    //   console.log(toDosCopy);
+    //   // 2) Put back the item on the destination.index
+    //   console.log("Put back", draggableId, "on ", destination.index);
+    //   toDosCopy.splice(destination?.index, 0, draggableId);
+    //   console.log(toDosCopy);
+    return;
+    // });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                ))}
-
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
-          );
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
